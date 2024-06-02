8c0f6803728074c946b6da958f3fd076481446a2
Maksymilian Majek <majek.maks@wp.pl>
2024-06-02 09:51:03 +0200

    zad1

--- pp-lab-3/src/main/java/org/example/Main.java
+++ pp-lab-3/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.awt.*;
+import java.io.Console;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point point = new Point(10,10);
+        System.out.printf("Point coords: x:%s : y:%s%n",point.getX(), point.getY());
     }
 }
